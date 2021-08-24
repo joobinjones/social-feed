aad65a8701b9102c6fcca3874a059a9db2963e52
Austin Jones` <austinhjones3@gmail.com>
2021-08-23 20:37:50 -0500

    format form

--- src/components/AddEditPost.tsx
+++ src/components/AddEditPost.tsx
@@ -4,7 +4,7 @@ import { IPost, addPost } from "../context/postsReducer";
 import { Box, Button } from "@chakra-ui/react";
 
 const AddEditPost = (): JSX.Element => {
-  const [{ posts, numOfPosts, user }, dispatch] = useStore();
+  const [{ numOfPosts, user }, dispatch] = useStore();
   const handleSubmit = ({ body, title }: { body: string; title: string }) => {
     const newPost: IPost = {
       body,
@@ -21,16 +21,22 @@ const AddEditPost = (): JSX.Element => {
     <Formik initialValues={{ body: "", title: "" }} onSubmit={handleSubmit}>
       <Form>
         <Box d="flex" flexDirection="column">
-          <label htmlFor="title">Title</label>
-          <Field type="text" id="title" name="title" placeholder="Post title" />
-          <label htmlFor="body">Your Post...</label>
-          <Field
-            as="textarea"
-            id="body"
-            name="body"
-            placeholder="Start typing something useless that you would like to share with random strangers for no apparent reason..."
-          />
-          <button type="submit">Submit</button>
+          <Box mt="10" d="flex" flexDirection="column" width="300px">
+            <label htmlFor="title">Title</label>
+            <Field type="text" id="title" name="title" placeholder="Post title" />
+          </Box>
+          <Box mt="10" d="flex" flexDirection="column" width="300px">
+            <label htmlFor="body">Your Post...</label>
+            <Field
+              as="textarea"
+              id="body"
+              name="body"
+              placeholder="Start typing something useless that you would like to share with random strangers for no apparent reason..."
+            />
+          </Box>
+          <Button mt="10" width="100px" type="submit">
+            Submit
+          </Button>
         </Box>
       </Form>
     </Formik>
